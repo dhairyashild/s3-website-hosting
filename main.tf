3f4e6f5e85883e52e39661c03f16bbed935b9d26
DHAIRYASHIL DESAI <dhairyashild300@gmail.com>
2025-10-15 11:23:46 +0530

    Fix Route 53 conflict - use mumbai subdomain
    - Changed from s3web.desai-devops.info to mumbai.desai-devops.info
    - Avoids conflict with existing Route 53 record
    - Simple S3 static website in Mumbai region
    - No CloudFront, just simple S3 hosting
    
    New URL: http://mumbai.desai-devops.info

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ data "aws_route53_zone" "primary" {
   name = "desai-devops.info."
 }
 
-# S3 Bucket for static website hosting in Mumbai region
+# Simple S3 Bucket for static website hosting in Mumbai
 resource "aws_s3_bucket" "static_website" {
-  bucket = "s3web-ap-south-1.desai-devops.info"
+  bucket = "s3web-mumbai.desai-devops.info"
 }
 
 # Enable static website hosting
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_website_configuration" "static_website" {
   }
 }
 
-# Configure public access block
+# Configure public access
 resource "aws_s3_bucket_public_access_block" "static_website" {
   bucket = aws_s3_bucket.static_website.id
 
@@ -44,7 +44,7 @@ resource "aws_s3_bucket_public_access_block" "static_website" {
   restrict_public_buckets = false
 }
 
-# Set bucket policy for public read access
+# Simple bucket policy for public read
 resource "aws_s3_bucket_policy" "static_website" {
   bucket = aws_s3_bucket.static_website.id
   depends_on = [aws_s3_bucket_public_access_block.static_website]
@@ -68,7 +68,6 @@ resource "aws_s3_object" "index_html" {
   key          = "index.html"
   source       = "index.html"
   content_type = "text/html"
-  depends_on   = [aws_s3_bucket_policy.static_website]
 }
 
 # Upload error.html
@@ -77,89 +76,17 @@ resource "aws_s3_object" "error_html" {
   key          = "error.html"
   source       = "error.html"
   content_type = "text/html"
-  depends_on   = [aws_s3_bucket_policy.static_website]
 }
 
-# Create Route 53 record pointing to Mumbai S3 website
+# Route 53 record for Mumbai S3 - using different subdomain
 resource "aws_route53_record" "website_mumbai" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "mumbai.desai-devops.info"
+  name    = "mumbai.desai-devops.info"  # Changed from s3web to mumbai
   type    = "A"
 
   alias {
     name                   = aws_s3_bucket_website_configuration.static_website.website_domain
     zone_id                = aws_s3_bucket.static_website.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-# Optional: Create CloudFront distribution for better global performance
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.static_website.bucket_regional_domain_name
-    origin_id   = "S3-mumbai-${aws_s3_bucket.static_website.bucket}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "CloudFront distribution for Mumbai S3 static website"
-  default_root_object = "index.html"
-
-  aliases = ["cdn.desai-devops.info"]
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-mumbai-${aws_s3_bucket.static_website.bucket}"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  price_class = "PriceClass_200"  # Includes Asia regions
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  tags = {
-    Name        = "mumbai-s3-website-distribution"
-    Environment = "production"
-    Region      = "ap-south-1"
-  }
-}
-
-# Route 53 record for CloudFront
-resource "aws_route53_record" "cloudfront" {
-  zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "cdn.desai-devops.info"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,19 @@
-output "mumbai_s3_website_endpoint" {
-  description = "Mumbai S3 website endpoint"
-  value       = "http://${aws_s3_bucket_website_configuration.static_website.website_endpoint}"
-}
-
-output "mumbai_domain_url" {
-  description = "Mumbai region domain URL"
+output "website_url" {
+  description = "Website URL"
   value       = "http://mumbai.desai-devops.info"
 }
 
-output "cloudfront_url" {
-  description = "CloudFront distribution URL"
-  value       = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+output "s3_endpoint" {
+  description = "S3 website endpoint"
+  value       = "http://${aws_s3_bucket_website_configuration.static_website.website_endpoint}"
 }
 
-output "cdn_domain_url" {
-  description = "CDN domain URL"
-  value       = "https://cdn.desai-devops.info"
-}
-
-output "s3_bucket_name" {
+output "bucket_name" {
   description = "S3 bucket name"
   value       = aws_s3_bucket.static_website.bucket
 }
 
-output "cloudfront_distribution_id" {
-  description = "CloudFront distribution ID"
-  value       = aws_cloudfront_distribution.s3_distribution.id
-}
-
-output "route53_zone_id" {
-  description = "Route 53 hosted zone ID"
-  value       = data.aws_route53_zone.primary.zone_id
+output "region" {
+  description = "AWS region"
+  value       = "ap-south-1"
 }
